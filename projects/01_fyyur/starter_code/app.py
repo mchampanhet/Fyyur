cd137363a41ded573845e62490ac8ae4b64145ba
Matt Champanhet <mchampanhet@moody.edu>
2021-03-27 16:58:37 -0500

    fix the db seeding

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -9,6 +9,7 @@ from flask import Flask, render_template, request, Response, flash, redirect, ur
 from flask_moment import Moment
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+import psycopg2
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
@@ -79,134 +80,142 @@ class Show(db.Model):
   artist_image_link = db.Column(db.String(500))
   start_time = db.Column(db.DateTime, nullable=False)
 
-# uncomment the following block to seed the db the very first time
-'''
-venue1 = Venue(
-  name='The Musical Hop',
-  genres=['Jazz','Reggae','Swing','Classical','Folk'],
-  address='1015 Folsom Street',
-  city='San Francisco',
-  state='CA',
-  phone='123-123-1234',
-  website='https://www.themusicalhop.com',
-  facebook_link='https://www.facebook.com/TheMusicalHop',
-  seeking_talent=True,
-  seeking_description='We are on the lookout for a local artist to play every two weeks. Please call us.',
-  image_link='https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
-)
-venue2 = Venue(
-  name='The Dueling Pianos Bar',
-  genres=['Classical','R&B','Hip-Hop'],
-  address='336 Delancey Street',
-  city='New York',
-  state='NY',
-  phone='914-003-1132',
-  website='https://www.theduelingpianos.com',
-  facebook_link='https://www.facebook.com/theduelingpianos',
-  seeking_talent=False,
-  image_link='https://images.unsplash.com/photo-1497032205916-ac775f0649ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
-)
-venue3 = Venue(
-  name='Park Square Live Music & Coffee',
-  genres=['Rock n Roll','Jazz','Classical','Folk'],
-  address='34 Whiskey Moore Ave',
-  city='San Francisco',
-  state='CA',
-  phone='415-000-1234',
-  website='https://www.parksquarelivemusicandcoffee.com',
-  facebook_link='https://www.facebook.com/ParkSquareLiveMusicAndCoffee',
-  seeking_talent=False,
-  image_link='https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80'
-)
-artist1 = Artist(
-  name='Guns n Petals',
-  genres=['Rock n Roll'],
-  city='San Francisco',
-  state='CA',
-  phone='326-123-5000',
-  website='https://www.gunsnpetalsband.com',
-  facebook_link='https://www.facebook.com/GunsNPetals',
-  seeking_venue=True,
-  seeking_description='Looking for shows to perform at in the San Francisco Bay Area!',
-  image_link='https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80'
-)
-artist2 = Artist(
-  name='Matt Quevedo',
-  genres=['Jazz'],
-  city='New York',
-  state='NY',
-  phone='300-400-5000',
-  facebook_link='https://www.facebook.com/mattquevedo923251523',
-  seeking_venue=False,
-  image_link='https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
-)
-artist3 = Artist(
-  name='The Wild Sax Band',
-  genres=['Jazz','Classical'],
-  city='San Francisco',
-  state='CA',
-  phone='432-325-5432',
-  seeking_venue=False,
-  image_link='https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80'
-)
-show1 = Show(
-  venue_id=1,
-  venue_name='The Musical Hop',
-  venue_image_link='https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
-  artist_id=1,
-  artist_name='Guns N Petals',
-  artist_image_link='https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
-  start_time='2019-05-21T21:30:00.000Z'
-)
-show2 = Show(
-  venue_id=3,
-  venue_name='Park Square Live Music & Coffee',
-  venue_image_link='https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80',
-  artist_id=2,
-  artist_name='Matt Quevedo',
-  artist_image_link='https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
-  start_time='2019-06-15T23:00:00.000Z'
-)
-show3 = Show(
-  venue_id=3,
-  venue_name='Park Square Live Music & Coffee',
-  venue_image_link='https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80',
-  artist_id=3,
-  artist_name='The Wild Sax Band',
-  artist_image_link='https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80',
-  start_time='2035-04-01T20:00:00.000Z'
-)
-show4 = Show(
-  venue_id=3,
-  venue_name='Park Square Live Music & Coffee',
-  venue_image_link='https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80',
-  artist_id=3,
-  artist_name='The Wild Sax Band',
-  artist_image_link='https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80',
-  start_time='2035-04-08T20:00:00.000Z'
-)
-show5 = Show(
-  venue_id=3,
-  venue_name='Park Square Live Music & Coffee',
-  venue_image_link='https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80',
-  artist_id=3,
-  artist_name='The Wild Sax Band',
-  artist_image_link='https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80',
-  start_time='2035-04-15T20:00:00.000Z'
-)
-db.session.add(venue1)
-db.session.add(venue2)
-db.session.add(venue3)
-db.session.add(artist1)
-db.session.add(artist2)
-db.session.add(artist3)
-db.session.add(show1)
-db.session.add(show2)
-db.session.add(show3)
-db.session.add(show4)
-db.session.add(show5)
-db.session.commit()
-'''
+conn = psycopg2.connect(app.config['SQLALCHEMY_DATABASE_URI'])
+cursor = conn.cursor()
+cursor.execute('select count(*) as count from pg_tables where tablename in (\'Venue\', \'Show\', \'Artist\')')
+row = cursor.fetchone()
+print(row[0])
+if row[0] == 3:
+  cursor.execute('select count(a.id) as count from (select id from "Artist" b union select id from "Show" c union select id from "Venue" d) a')
+  row = cursor.fetchone()
+  if row[0] == 0:
+
+    venue1 = Venue(
+      name='The Musical Hop',
+      genres=['Jazz','Reggae','Swing','Classical','Folk'],
+      address='1015 Folsom Street',
+      city='San Francisco',
+      state='CA',
+      phone='123-123-1234',
+      website='https://www.themusicalhop.com',
+      facebook_link='https://www.facebook.com/TheMusicalHop',
+      seeking_talent=True,
+      seeking_description='We are on the lookout for a local artist to play every two weeks. Please call us.',
+      image_link='https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
+    )
+    venue2 = Venue(
+      name='The Dueling Pianos Bar',
+      genres=['Classical','R&B','Hip-Hop'],
+      address='336 Delancey Street',
+      city='New York',
+      state='NY',
+      phone='914-003-1132',
+      website='https://www.theduelingpianos.com',
+      facebook_link='https://www.facebook.com/theduelingpianos',
+      seeking_talent=False,
+      image_link='https://images.unsplash.com/photo-1497032205916-ac775f0649ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+    )
+    venue3 = Venue(
+      name='Park Square Live Music & Coffee',
+      genres=['Rock n Roll','Jazz','Classical','Folk'],
+      address='34 Whiskey Moore Ave',
+      city='San Francisco',
+      state='CA',
+      phone='415-000-1234',
+      website='https://www.parksquarelivemusicandcoffee.com',
+      facebook_link='https://www.facebook.com/ParkSquareLiveMusicAndCoffee',
+      seeking_talent=False,
+      image_link='https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80'
+    )
+    artist1 = Artist(
+      name='Guns n Petals',
+      genres=['Rock n Roll'],
+      city='San Francisco',
+      state='CA',
+      phone='326-123-5000',
+      website='https://www.gunsnpetalsband.com',
+      facebook_link='https://www.facebook.com/GunsNPetals',
+      seeking_venue=True,
+      seeking_description='Looking for shows to perform at in the San Francisco Bay Area!',
+      image_link='https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80'
+    )
+    artist2 = Artist(
+      name='Matt Quevedo',
+      genres=['Jazz'],
+      city='New York',
+      state='NY',
+      phone='300-400-5000',
+      facebook_link='https://www.facebook.com/mattquevedo923251523',
+      seeking_venue=False,
+      image_link='https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
+    )
+    artist3 = Artist(
+      name='The Wild Sax Band',
+      genres=['Jazz','Classical'],
+      city='San Francisco',
+      state='CA',
+      phone='432-325-5432',
+      seeking_venue=False,
+      image_link='https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80'
+    )
+    show1 = Show(
+      venue_id=1,
+      venue_name='The Musical Hop',
+      venue_image_link='https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+      artist_id=1,
+      artist_name='Guns N Petals',
+      artist_image_link='https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+      start_time='2019-05-21T21:30:00.000Z'
+    )
+    show2 = Show(
+      venue_id=3,
+      venue_name='Park Square Live Music & Coffee',
+      venue_image_link='https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80',
+      artist_id=2,
+      artist_name='Matt Quevedo',
+      artist_image_link='https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      start_time='2019-06-15T23:00:00.000Z'
+    )
+    show3 = Show(
+      venue_id=3,
+      venue_name='Park Square Live Music & Coffee',
+      venue_image_link='https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80',
+      artist_id=3,
+      artist_name='The Wild Sax Band',
+      artist_image_link='https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80',
+      start_time='2035-04-01T20:00:00.000Z'
+    )
+    show4 = Show(
+      venue_id=3,
+      venue_name='Park Square Live Music & Coffee',
+      venue_image_link='https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80',
+      artist_id=3,
+      artist_name='The Wild Sax Band',
+      artist_image_link='https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80',
+      start_time='2035-04-08T20:00:00.000Z'
+    )
+    show5 = Show(
+      venue_id=3,
+      venue_name='Park Square Live Music & Coffee',
+      venue_image_link='https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80',
+      artist_id=3,
+      artist_name='The Wild Sax Band',
+      artist_image_link='https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80',
+      start_time='2035-04-15T20:00:00.000Z'
+    )
+    db.session.add(venue1)
+    db.session.add(venue2)
+    db.session.add(venue3)
+    db.session.add(artist1)
+    db.session.add(artist2)
+    db.session.add(artist3)
+    db.session.add(show1)
+    db.session.add(show2)
+    db.session.add(show3)
+    db.session.add(show4)
+    db.session.add(show5)
+    db.session.commit()
+
 
 #----------------------------------------------------------------------------#
 # Filters.
